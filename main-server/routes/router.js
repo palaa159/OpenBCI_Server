94aaa2b06581da6af42b0b4f300e81e87071c64e
root <root@ip-172-31-30-38.us-west-2.compute.internal>
2014-12-17 18:40:52 +0000

    update

---
+++ main-server/routes/router.js
@@ -0,0 +1,50 @@
+/* 
+	ROUTER.JS
+	routing everthingy
+*/
+var community = require('./community');
+
+module.exports = function(router, util, bodyParser, moment, github) {
+    router.use(function(req, res, next) {
+        console.log('-------------------------'.white);
+        console.log('ROUTING STARTS'.bold);
+        console.log('-------------------------'.white);
+        console.log(req.method, util.inspect(req.url));
+        req.url.toLowerCase();
+        next();
+    });
+
+    // ROUTE TO COMMUNITY
+    router.route('/community')
+        .get(function(req, res) {
+            community.wpGetPosts(function(posts) {
+                res.render('clients/community', {
+                    title: 'OpenBCI | Community',
+                    data: posts
+                });
+            });
+        });
+
+    // ROUTE TO COMMUNITY BLOG
+    router.route('/community/:id')
+        .get(function(req, res) {
+            // console.log(req.params);
+            var idToQuery = req.params.id;
+            community.wpGetPostFromId(idToQuery, function(post) {
+                res.render('clients/wp-post', {
+                    title: post.title,
+                    post: post,
+                    timeAgo: moment(post.date).fromNow()
+                });
+            });
+        });
+
+    /* SITE UTILS
+     */
+
+    // 404
+    router.route('*')
+        .all(function(req, res) {
+            res.send('404 Page.');
+        });
+};
